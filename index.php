ccc13e1d841ca5fff3c18ae8c8ead62499c1f13a
Krs <christian.lopez.be@gmail.com>
2020-07-10 17:47:00 -0500

    Erase var_dump comments.

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
+/**
+ * @todo make error reporting only on dev and debuging mode. Add .env functionality.
+ */
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
 require __DIR__ . '/vendor/autoload.php';
-//var_dump(__DIR__ . '/bootstrap.php');
+
 require __DIR__ . '/bootstrap.php';
