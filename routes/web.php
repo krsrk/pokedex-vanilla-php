8222b691ece20b8087ed6b1ac1949730f2604f91
Krs <christian.lopez.be@gmail.com>
2020-09-07 14:45:59 -0500

    adding default routes.

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,12 @@
 
 $route = new \Utils\Routing\Route(new \Utils\Request($_SERVER['REQUEST_URI']));
 
+$route->add('/', function () {
+    echo '<h1>Hello Pokedex!</h1>';
+});
+
+$route->add('/str', 'Pikachu!!!');
+
+$route->add('/arr', ['pokemon' => 'Charizard']);
+
 $route->add('/id/:id', 'PokemonController@index');
