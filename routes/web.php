21c2452e8b2831ecc3d526b4eff483a4b691312b
Krs <christian.lopez.be@gmail.com>
2020-10-13 14:57:08 -0500

    modify api route to use Pokemon model.

--- routes/web.php
+++ routes/web.php
@@ -18,13 +18,15 @@ $route->add('/arr', ['pokemon' => 'Charizard']);
 $route->add('/id/:id', 'PokemonController@index');
 
 $route->add('/api', function () {
-    $data = [
+    /*$data = [
         ['codigo' => '001', 'pokemon' => 'Bulbasaur'],
         ['codigo' => '002', 'pokemon' => 'Ivysaur'],
         ['codigo' => '003', 'pokemon' => 'Venusaur'],
         ['codigo' => '004', 'pokemon' => 'Charmander'],
         ['codigo' => '005', 'pokemon' => 'Charmeleon'],
-    ];
+    ];*/
+
+    $data = \App\Models\Pokemon::all()->toArray();
 
     response()->json($data, 201);
 });
