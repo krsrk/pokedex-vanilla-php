22b6d79b3c9cabdaf1f7c8ec51201a6dd073aa7d
Krs <christian.lopez.be@gmail.com>
2020-10-27 08:58:24 -0600

    add TheSearchBox.vue component

---
+++ resources/js/components/TheSearchBox.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="m-5">
+        <input
+            type="search"
+            name="query"
+            placeholder="Find your Pokemon ..."
+            required="required"
+            class="p-4 text-gray-700 w-full bg-white border border-gray-300 rounded-lg focus:outline-none focus:border-gray-400">
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TheSearchBox"
+}
+</script>
