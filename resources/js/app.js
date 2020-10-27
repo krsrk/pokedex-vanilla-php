df4fdd61384339ef0e7bd2d7b094099b293f945d
Krs <christian.lopez.be@gmail.com>
2020-10-27 08:59:14 -0600

    adding data from controller to components

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,19 @@
 import Vue from "vue"
 
+import TheNav from "./components/TheNav"
+import TheSearchBox from "./components/TheSearchBox"
+import TheListData from "./components/TheListData"
+import TheFooter from "./components/TheFooter"
+
 new Vue({
     el: "#app",
     delimiters: ['${', '}'],
+    components: {
+        'the-nav': TheNav,
+        'the-search-box': TheSearchBox,
+        'the-list-data': TheListData,
+        'the-footer': TheFooter,
+    },
     data () {
         return {
             message: "Hello Pokedex!!!"
